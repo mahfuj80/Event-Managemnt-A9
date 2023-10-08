3319dff37dac1f60c8c302d43fd37152cdc018f9
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-08 14:54:01 +0600

    json data edited and dynamcally displayed card

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from 'react';
 const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch('services.json')
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  console.log(services);
   return (
     <section className="pt-20 pb-12 lg:pt-[120px] lg:pb-[90px] px-8 md:px-14">
       <div className="container mx-auto">
@@ -19,26 +27,32 @@ const Services = () => {
           </div>
         </div>
         <div className="-mx-4 flex flex-wrap justify-center">
-          <div className="w-full px-4 sm:w-4/5 md:w-1/2 lg:w-1/3">
-            <div className="group relative mb-8 rounded-xl border-2 border-[#b9b9b9] bg-white p-10 text-center md:px-8 lg:py-9 lg:px-6 xl:p-10 2xl:p-12">
-              <div className="relative z-10 mx-auto inline-flex items-center">
-                <img
-                  className="w-full h-full"
-                  src="/public/images/anniversary.png"
-                  alt="logo"
-                />
+          {services.map((service) => (
+            <>
+              <div className="w-full px-4 sm:w-4/5 md:w-1/2 lg:w-1/3">
+                <div className="hover:shadow-xl relative mb-8 rounded-xl border-2 border-[#b9b9b9] bg-white p-10 text-center md:px-8 lg:py-9 lg:px-6 xl:p-10 2xl:p-12">
+                  <div className="relative z-10 mx-auto inline-flex items-center">
+                    <img
+                      className="w-full h-full"
+                      src={service.logo}
+                      alt="logo"
+                    />
+                  </div>
+                  <h4 className="mb-3 text-xl font-semibold text-dark">
+                    {service.name}
+                  </h4>
+                  <p className="text-xl">
+                    Price:
+                    <span className="text-green-600 font-bold">
+                      ${service.price}
+                    </span>
+                  </p>
+                  <p className="text-body-color">{service.short_description}</p>
+                  <button className="btn btn-primary my-2">Details</button>
+                </div>
               </div>
-              <h4 className="mb-3 text-xl font-semibold text-dark">
-                Marketing Solutions
-              </h4>
-              <p className="text-body-color">
-                Lorem Ipsum is simply dummy text of the printing and industry.
-              </p>
-              <button className="btn btn-primary my-2">Details</button>
-
-              <span className="absolute -bottom-2 left-0 right-0 z-[-1] mx-auto h-12 w-3/4 bg-primary opacity-0 blur-[12px] transition group-hover:opacity-[14%]"></span>
-            </div>
-          </div>
+            </>
+          ))}
         </div>
       </div>
     </section>
