dd77b3411f3335e751bbd7485b8a201c410c6bb6
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-08 21:28:53 +0600

    firebase installed and env.locl file added

--- src/Firebase/Firebase.jsx
+++ src/Firebase/Firebase.jsx
@@ -5,14 +5,13 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const all = import.meta.env;
-console.log(all);
 const firebaseConfig = {
-  apiKey: 'AIzaSyCJOdNbrSLYIhyPcAd-Z4z5Q0_CC_w2-Q8',
-  authDomain: 'event-management-a9-7fba6.firebaseapp.com',
-  projectId: 'event-management-a9-7fba6',
-  storageBucket: 'event-management-a9-7fba6.appspot.com',
-  messagingSenderId: '804434449941',
-  appId: '1:804434449941:web:06fd2871dcee5fa3e3e01c',
+  apiKey: all.VITE_APIKEY,
+  authDomain: all.VITE_AUTHDOMAIN,
+  projectId: all.VITE_PROJECTID,
+  storageBucket: all.VITE_STORAGEBUCKET,
+  messagingSenderId: all.VITE_MESSAGINGSENDERID,
+  appId: all.VITE_APPID,
 };
 
 // Initialize Firebase
