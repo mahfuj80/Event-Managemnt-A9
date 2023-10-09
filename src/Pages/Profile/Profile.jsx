14c63fc477c97fb9053968178ab34de3ea6ac7a1
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-09 16:29:38 +0600

    prfile page added and review data changed

---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div className="w-full flex justify-center items-center h-[90vh]">
+      <h1 className="text-5xl font-bold">Profile Is Coming</h1>
+    </div>
+  );
+};
+
+export default Profile;
--- src/Pages/Reviews/Reviews.jsx
+++ src/Pages/Reviews/Reviews.jsx
@@ -10,14 +10,14 @@ const Reviews = () => {
             <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
               <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
                 <p>
-                  I have no design skills and with Tailwind I can actually make
-                  good looking websites with ease and it is everything I ever
-                  wanted in a CSS framework.
+                  Attended a tech expo and was blown away by the innovation
+                  showcased. The event was well-organized, and the exhibits were
+                  both educational and inspiring.
                 </p>
               </blockquote>
               <figcaption className="flex items-center space-x-4">
                 <img
-                  src="/_next/static/media/ryan-florence.3af9c9d9.jpg"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                   alt=""
                   className="flex-none w-14 h-14 rounded-full object-cover"
                   loading="lazy"
@@ -26,7 +26,7 @@ const Reviews = () => {
                 <div className="flex-auto">
                   <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
                     <a
-                      href="https://twitter.com/ryanflorence/status/1187951799442886656"
+                      href="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                       tabIndex="0"
                     >
                       <span className="absolute inset-0"></span>Ryan Florence
@@ -41,14 +41,14 @@ const Reviews = () => {
             <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
               <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
                 <p>
-                  I have no design skills and with Tailwind I can actually make
-                  good looking websites with ease and it is everything I ever
-                  wanted in a CSS framework.
+                  The gaming convention was an absolute blast! From the latest
+                  game releases to engaging tournaments, it was a paradise for
+                  gamers. Well-done organizers!
                 </p>
               </blockquote>
               <figcaption className="flex items-center space-x-4">
                 <img
-                  src="/_next/static/media/ryan-florence.3af9c9d9.jpg"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                   alt=""
                   className="flex-none w-14 h-14 rounded-full object-cover"
                   loading="lazy"
@@ -57,7 +57,7 @@ const Reviews = () => {
                 <div className="flex-auto">
                   <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
                     <a
-                      href="https://twitter.com/ryanflorence/status/1187951799442886656"
+                      href="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                       tabIndex="0"
                     >
                       <span className="absolute inset-0"></span>Ryan Florence
@@ -72,14 +72,14 @@ const Reviews = () => {
             <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
               <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
                 <p>
-                  I have no design skills and with Tailwind I can actually make
-                  good looking websites with ease and it is everything I ever
-                  wanted in a CSS framework.
+                  The speaker at the conference was truly remarkable. Their
+                  insights and passion for the subject left a lasting impact on
+                  all attendees.
                 </p>
               </blockquote>
               <figcaption className="flex items-center space-x-4">
                 <img
-                  src="/_next/static/media/ryan-florence.3af9c9d9.jpg"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                   alt=""
                   className="flex-none w-14 h-14 rounded-full object-cover"
                   loading="lazy"
@@ -88,96 +88,7 @@ const Reviews = () => {
                 <div className="flex-auto">
                   <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
                     <a
-                      href="https://twitter.com/ryanflorence/status/1187951799442886656"
-                      tabIndex="0"
-                    >
-                      <span className="absolute inset-0"></span>Ryan Florence
-                    </a>
-                  </div>
-                  <div className="mt-0.5">Remix &amp; React Training</div>
-                </div>
-              </figcaption>
-            </figure>
-          </li>
-        </ul>
-        <ul className="space-y-8">
-          <li className="text-sm leading-6">
-            <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
-              <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
-                <p>
-                  I feel like an idiot for not using Tailwind CSS until now.
-                </p>
-              </blockquote>
-              <figcaption className="flex items-center space-x-4">
-                <img
-                  src="/_next/static/media/ryan-florence.3af9c9d9.jpg"
-                  alt=""
-                  className="flex-none w-14 h-14 rounded-full object-cover"
-                  loading="lazy"
-                  decoding="async"
-                />
-                <div className="flex-auto">
-                  <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
-                    <a
-                      href="https://twitter.com/ryanflorence/status/1187951799442886656"
-                      tabIndex="0"
-                    >
-                      <span className="absolute inset-0"></span>Ryan Florence
-                    </a>
-                  </div>
-                  <div className="mt-0.5">Remix &amp; React Training</div>
-                </div>
-              </figcaption>
-            </figure>
-          </li>
-          <li className="text-sm leading-6">
-            <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
-              <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
-                <p>
-                  I feel like an idiot for not using Tailwind CSS until now.
-                </p>
-              </blockquote>
-              <figcaption className="flex items-center space-x-4">
-                <img
-                  src="/_next/static/media/ryan-florence.3af9c9d9.jpg"
-                  alt=""
-                  className="flex-none w-14 h-14 rounded-full object-cover"
-                  loading="lazy"
-                  decoding="async"
-                />
-                <div className="flex-auto">
-                  <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
-                    <a
-                      href="https://twitter.com/ryanflorence/status/1187951799442886656"
-                      tabIndex="0"
-                    >
-                      <span className="absolute inset-0"></span>Ryan Florence
-                    </a>
-                  </div>
-                  <div className="mt-0.5">Remix &amp; React Training</div>
-                </div>
-              </figcaption>
-            </figure>
-          </li>
-          <li className="text-sm leading-6">
-            <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
-              <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
-                <p>
-                  I feel like an idiot for not using Tailwind CSS until now.
-                </p>
-              </blockquote>
-              <figcaption className="flex items-center space-x-4">
-                <img
-                  src="/_next/static/media/ryan-florence.3af9c9d9.jpg"
-                  alt=""
-                  className="flex-none w-14 h-14 rounded-full object-cover"
-                  loading="lazy"
-                  decoding="async"
-                />
-                <div className="flex-auto">
-                  <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
-                    <a
-                      href="https://twitter.com/ryanflorence/status/1187951799442886656"
+                      href="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                       tabIndex="0"
                     >
                       <span className="absolute inset-0"></span>Ryan Florence
@@ -194,12 +105,14 @@ const Reviews = () => {
             <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
               <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
                 <p>
-                  I feel like an idiot for not using Tailwind CSS until now.
+                  Attended a product launch event and the unveiling was
+                  flawless. The products features were presented seamlessly,
+                  generating excitement and anticipation.
                 </p>
               </blockquote>
               <figcaption className="flex items-center space-x-4">
                 <img
-                  src="/_next/static/media/ryan-florence.3af9c9d9.jpg"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                   alt=""
                   className="flex-none w-14 h-14 rounded-full object-cover"
                   loading="lazy"
@@ -208,7 +121,38 @@ const Reviews = () => {
                 <div className="flex-auto">
                   <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
                     <a
-                      href="https://twitter.com/ryanflorence/status/1187951799442886656"
+                      href="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                      tabIndex="0"
+                    >
+                      <span className="absolute inset-0"></span>Ryan Florence
+                    </a>
+                  </div>
+                  <div className="mt-0.5">Remix &amp; React Training</div>
+                </div>
+              </figcaption>
+            </figure>
+          </li>
+          <li className="text-sm leading-6">
+            <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+              <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
+                <p>
+                  The cultural celebration was vibrant and immersive. It
+                  provided a glimpse into diverse cultures and traditions,
+                  promoting unity and understanding.
+                </p>
+              </blockquote>
+              <figcaption className="flex items-center space-x-4">
+                <img
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  alt=""
+                  className="flex-none w-14 h-14 rounded-full object-cover"
+                  loading="lazy"
+                  decoding="async"
+                />
+                <div className="flex-auto">
+                  <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
+                    <a
+                      href="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                       tabIndex="0"
                     >
                       <span className="absolute inset-0"></span>Ryan Florence
@@ -228,7 +172,7 @@ const Reviews = () => {
               </blockquote>
               <figcaption className="flex items-center space-x-4">
                 <img
-                  src="/_next/static/media/ryan-florence.3af9c9d9.jpg"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                   alt=""
                   className="flex-none w-14 h-14 rounded-full object-cover"
                   loading="lazy"
@@ -237,7 +181,40 @@ const Reviews = () => {
                 <div className="flex-auto">
                   <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
                     <a
-                      href="https://twitter.com/ryanflorence/status/1187951799442886656"
+                      href="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                      tabIndex="0"
+                    >
+                      <span className="absolute inset-0"></span>Ryan Florence
+                    </a>
+                  </div>
+                  <div className="mt-0.5">Remix &amp; React Training</div>
+                </div>
+              </figcaption>
+            </figure>
+          </li>
+        </ul>
+        <ul className="space-y-8">
+          <li className="text-sm leading-6">
+            <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+              <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
+                <p>
+                  Participated in an educational webinar that was informative
+                  and engaging. The speakers were knowledgeable, making the
+                  session enriching.
+                </p>
+              </blockquote>
+              <figcaption className="flex items-center space-x-4">
+                <img
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  alt=""
+                  className="flex-none w-14 h-14 rounded-full object-cover"
+                  loading="lazy"
+                  decoding="async"
+                />
+                <div className="flex-auto">
+                  <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
+                    <a
+                      href="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                       tabIndex="0"
                     >
                       <span className="absolute inset-0"></span>Ryan Florence
@@ -252,12 +229,14 @@ const Reviews = () => {
             <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
               <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
                 <p>
-                  I feel like an idiot for not using Tailwind CSS until now.
+                  The wellness expo was rejuvenating. From yoga sessions to
+                  wellness tips, it was a holistic experience for the mind and
+                  body.
                 </p>
               </blockquote>
               <figcaption className="flex items-center space-x-4">
                 <img
-                  src="/_next/static/media/ryan-florence.3af9c9d9.jpg"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                   alt=""
                   className="flex-none w-14 h-14 rounded-full object-cover"
                   loading="lazy"
@@ -266,7 +245,38 @@ const Reviews = () => {
                 <div className="flex-auto">
                   <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
                     <a
-                      href="https://twitter.com/ryanflorence/status/1187951799442886656"
+                      href="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                      tabIndex="0"
+                    >
+                      <span className="absolute inset-0"></span>Ryan Florence
+                    </a>
+                  </div>
+                  <div className="mt-0.5">Remix &amp; React Training</div>
+                </div>
+              </figcaption>
+            </figure>
+          </li>
+          <li className="text-sm leading-6">
+            <figure className="relative flex flex-col-reverse bg-slate-50 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+              <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
+                <p>
+                  The charity gala was a success, raising significant funds for
+                  a noble cause. The event had a heartwarming atmosphere and a
+                  sense of community.
+                </p>
+              </blockquote>
+              <figcaption className="flex items-center space-x-4">
+                <img
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  alt=""
+                  className="flex-none w-14 h-14 rounded-full object-cover"
+                  loading="lazy"
+                  decoding="async"
+                />
+                <div className="flex-auto">
+                  <div className="text-base text-slate-900 font-semibold dark:text-slate-300">
+                    <a
+                      href="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
                       tabIndex="0"
                     >
                       <span className="absolute inset-0"></span>Ryan Florence
