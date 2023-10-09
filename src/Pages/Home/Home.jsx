2b5dfc3d61307998c31ff556dbaa9f1b4ccf16b7
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-09 20:04:47 +0600

    aos add to the home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,25 +1,46 @@
-import { useContext } from 'react';
+import { useEffect } from 'react';
 import Carousel from '../../components/Carousel/Carousels';
 import Faq from '../../components/Faq/Faq';
 import Services from '../../components/Services/Services';
 import TeamMember from '../../components/TeamMember/TeamMember';
-import { AuthContext } from '../../Provider/AuthProvider';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const Home = () => {
-  const { name } = useContext(AuthContext);
-  console.log(name);
+  // const { name } = useContext(AuthContext);
+  // console.log(name);
+  useEffect(() => {
+    AOS.init({});
+  }, []);
   return (
     <main className="relative">
       {/* carousel Banner Section*/}
-      <section className="overflow-hidden md:overflow-auto">
+      <section
+        data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="1500"
+        className="overflow-hidden md:overflow-auto"
+      >
         <Carousel></Carousel>
       </section>
       {/* Services Section */}
 
-      <Services></Services>
-      <section className="bg-blend-darken">
+      <section data-aos="flip-right" data-aos-duration="1000">
+        <Services></Services>
+      </section>
+      <section
+        data-aos="flip-left"
+        data-aos-duration="500"
+        className="bg-blend-darken"
+      >
         <TeamMember></TeamMember>
       </section>
-      <Faq></Faq>
+      <section
+        data-aos="fade-right"
+        data-aos-easing="linear"
+        data-aos-duration="1500"
+      >
+        <Faq></Faq>
+      </section>
     </main>
   );
 };
