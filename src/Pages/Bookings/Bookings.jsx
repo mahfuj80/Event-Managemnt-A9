77ca1d4ec6730aef2fe23f258728c95f185df90c
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-09 14:47:00 +0600

    two more privet routes created

---
+++ src/Pages/Bookings/Bookings.jsx
@@ -0,0 +1,5 @@
+const Bookings = () => {
+  return <div>This Is Bookings Page</div>;
+};
+
+export default Bookings;
--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -5,6 +5,8 @@ import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import Registration from '../Pages/Registration/Registration';
 import EventDetails from '../Pages/EventDetails/EventDetails';
+import Bookings from '../Pages/Bookings/Bookings';
+import Reviews from '../Pages/Reviews/Reviews';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -22,6 +24,14 @@ const router = createBrowserRouter([
       {
         path: '/registration',
         element: <Registration></Registration>,
+      },
+      {
+        path: '/bookings',
+        element: <Bookings></Bookings>,
+      },
+      {
+        path: '/reviews',
+        element: <Reviews></Reviews>,
       },
       {
         path: '/details/:id',
