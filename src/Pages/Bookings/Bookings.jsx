1aeb1ca8cffec0111f7b0fe61f51e3d4b219dab7
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-09 16:15:15 +0600

    services price increased

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -52,111 +52,12 @@ const Bookings = () => {
                     United-State
                   </span>
                 </td>
-                <td>{service?.price}</td>
+                <td>{service?.price}$</td>
                 <th>
                   <button className="btn btn-ghost btn-xs">details</button>
                 </th>
               </tr>
             ))}
-            {/* row 2 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="https://i.ibb.co/5k4hW1p/anniversary.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Anniversary</div>
-                    <div className="text-sm opacity-50">Party</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                23 November 2023
-                <br />
-                <span className="badge badge-ghost badge-sm">United-State</span>
-              </td>
-              <td>5000$</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="https://i.ibb.co/k8Hw07p/baby-Shower.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Baby Shower</div>
-                    <div className="text-sm opacity-50">Los-Angels</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                23 November 2023
-                <br />
-                <span className="badge badge-ghost badge-sm">New-York</span>
-              </td>
-              <td>200$</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 4 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="https://i.ibb.co/kqsk6y5/birthday.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Birthday</div>
-                    <div className="text-sm opacity-50">Party</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                23 November 2023
-                <br />
-                <span className="badge badge-ghost badge-sm">Canada</span>
-              </td>
-              <td>300$</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
           </tbody>
           {/* foot */}
           <tfoot>
